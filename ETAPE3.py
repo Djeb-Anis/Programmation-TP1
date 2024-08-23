857369459056a226b3ba322dfcc5d2353528f63d
delange11 <gnzouakeu@gmail.com>
2024-08-22 21:11:28 -0400

    Debut etape 3

--- ETAPE3.py
+++ ETAPE3.py
@@ -1,9 +1,47 @@
-import sys
-import csv
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QTableWidget, QLineEdit, QPushButton, QVBoxLayout
 
-# Creation de l'objet AppNutrition de la classe QApplication
-AppNutrition = QApplication()
+########ETAPE-3#############
+import sys
+import csv    # pour lire et ecrire des fichiers cvs
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QTableWidget, QLineEdit, QPushButton,QGridLayout, QVBoxLayout, QTableWidgetItem
+def afficher():
+  print(edit_etape3.text())
+
+
+App = QApplication([])    # Creation de l'objet App de la classe QApplication
+
+fen_etape3 = QWidget() # Creation de l'objet fen de la classe QWidget
+fen_etape3.setGeometry(100, 100, 700, 300)
+fen_etape3.setWindowTitle("Application de Nutrition")
+
+#QGridLayout
+grid_etape3 = QGridLayout()
+fen_etape3.setLayout(grid_etape3)
+
+# QLineEdit
+edit_etape3 = QLineEdit(fen_etape3)
+grid_etape3.addWidget(edit_etape3, 0, 0)
+
+
+#QLabel
+qlbl = QLabel(fen_etape3)
+qlbl.setText("Veuillez insérer l'ID d'un aliment")
+grid_etape3.addWidget(qlbl, 0, 0)
+
+
+#QPushButton
+afficher_etape3 = QPushButton(fen_etape3)
+afficher_etape3.setText("Afficher les valeurs nutritives")
+afficher_etape3.clicked.connect(afficher)
+grid_etape3.addWidget(afficher_etape3, 1, 0)
+
+
+
+
+
+
+
+fen_etape3.show()
+App.exec()
 
 
 
